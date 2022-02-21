e439a641769afd98137cfb2605ebeb4a22daf200
Emily <emilyhigginson14@gmail.com>
2022-02-21 12:36:57 -0500

    commit tree dce38027672601533c209f6d78d570ef283584bd

--- methods.rb
+++ methods.rb
@@ -1,1 +1,25 @@
-# Your code here!
+def greet_programmer(programmer = "programmer")
+    
+    puts "Hello, #{programmer}!"
+end
+greet_programmer()
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+greet("Naureen")
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+    return num1 +num2 
+end
+
+def halve(integer)
+    if integer.class != Integer
+        return nil
+    end
+  integer/2
+end
